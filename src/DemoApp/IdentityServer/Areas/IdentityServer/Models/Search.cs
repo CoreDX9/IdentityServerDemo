9c5a2aa1c81d2b20d49127560d1228f6315ff559
CoreDX9 <coredx@live.cn>
2020-02-01 18:33:06 +0800

    增加 IdentityServer 管理（进行中…… 编译通过）

---
+++ src/DemoApp/IdentityServer/Areas/IdentityServer/Models/Search.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Areas.IdentityServer.Models
+{
+    public class Search
+    {
+        public string Action { get; set; }
+
+        public string Controller { get; set; }
+    }
+}
