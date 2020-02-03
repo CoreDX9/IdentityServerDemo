bce7d1870085f7c37898c755669782213a41bedd
CoreDX9 <coredx@live.cn>
2020-02-03 16:20:10 +0800

    整理代码

--- src/DemoApp/IdentityServer/Api/UsersController.cs
+++ src/DemoApp/IdentityServer/Api/UsersController.cs
@@ -7,8 +7,6 @@ using IdentityServer.Areas.Manage.Models.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using X.PagedList;
