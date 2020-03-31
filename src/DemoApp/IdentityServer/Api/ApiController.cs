37024aa2987c31e3eebf007b03404067bce55d8f
CoreDX9 <coredx@live.cn>
2020-04-01 02:20:41 +0800

    增加内存诊断示例

---
+++ src/DemoApp/IdentityServer/Api/ApiController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Api
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public ApiController()
+        {
+            Interlocked.Increment(ref DiagnosticsController.Requests);
+        }
+
+        private static ConcurrentBag<string> _staticStrings = new ConcurrentBag<string>();
+
+        [HttpGet("staticstring")]
+        public ActionResult<string> GetStaticString()
+        {
+            var bigString = new string('x', 10 * 1024);
+            _staticStrings.Add(bigString);
+            return bigString;
+        }
+
+        [HttpGet("bigstring")]
+        public ActionResult<string> GetBigString()
+        {
+            return new string('x', 10 * 1024);
+        }
+
+        [HttpGet("loh/{size=85000}")]
+        public int GetLOH(int size)
+        {
+            return new byte[size].Length;
+        }
+
+        private static readonly string TempPath = Path.GetTempPath();
+
+        [HttpGet("fileprovider")]
+        public void GetFileProvider()
+        {
+            var fp = new PhysicalFileProvider(TempPath);
+            fp.Watch("*.*");
+        }
+
+        [HttpGet("httpclient1")]
+        public async Task<int> GetHttpClient1(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync(url);
+                return (int)result.StatusCode;
+            }
+        }
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        [HttpGet("httpclient2")]
+        public async Task<int> GetHttpClient2(string url)
+        {
+            var result = await _httpClient.GetAsync(url);
+            return (int)result.StatusCode;
+        }
+
+        [HttpGet("array/{size}")]
+        public byte[] GetArray(int size)
+        {
+            var array = new byte[size];
+
+            var random = new Random();
+            random.NextBytes(array);
+
+            return array;
+        }
+
+        private static ArrayPool<byte> _arrayPool = ArrayPool<byte>.Create();
+
+        private class PooledArray : IDisposable
+        {
+            public byte[] Array { get; private set; }
+
+            public PooledArray(int size)
+            {
+                Array = _arrayPool.Rent(size);
+            }
+
+            public void Dispose()
+            {
+                _arrayPool.Return(Array);
+            }
+        }
+
+        [HttpGet("pooledarray/{size}")]
+        public byte[] GetPooledArray(int size)
+        {
+            var pooledArray = new PooledArray(size);
+
+            var random = new Random();
+            random.NextBytes(pooledArray.Array);
+
+            HttpContext.Response.RegisterForDispose(pooledArray);
+
+            return pooledArray.Array;
+        }
+
+    }
+}
